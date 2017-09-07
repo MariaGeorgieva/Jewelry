55115afde8546a177634adc97027747138d9b1c7
=MariaGeorgieva <=>
2017-09-07 22:40:39 +0300

    Fix Title

--- resources/views/sections/master.blade.php
+++ resources/views/sections/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>J.A.W. Special Offers NEW</title>
+    <title>J.A.W. Special Offers</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
